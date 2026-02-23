58864c6e5665752e601ff63e361dcde647a2a6d4
Dheeraj Kumar YMV (Deeraj Kumar YMV) <dheeruymv@gmail.com>
2026-02-23 16:53:29 +0530

    Vector DB Change
    - Created Pipeline -> Indexing
    - indexing.py will do following things
       (1) Read the files from data/
       (2) Perform chunking using langchain text_splitters
       (3) generate embeddings using chromadb & store to local disk (Persist to local disk)

---
+++ cc_bot/cc_bot_api/pipelines/indexing.py
@@ -0,0 +1,56 @@
+import chromadb
+from chromadb.config import Settings
+from pathlib import Path
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+DATA_PATH = "D:\My Git Hub\credit-card-bot\cc_bot\cc_bot_api\data\hdfc"
+
+def _read_data():
+    return Path(DATA_PATH).read_text()
+
+def _perform_chunking(data_to_chunk):
+    splitter = RecursiveCharacterTextSplitter(
+    chunk_size=50,
+    chunk_overlap=5
+    )
+    chunks = splitter.split_text(data_to_chunk)
+    return chunks
+
+class ChromaDB:
+
+    def __init__(self, collection_name, metadata, chunks):
+        self._collection_name = collection_name
+        self._metadata = metadata
+        self._chunks = chunks
+
+
+    def generate_embeddings(self):
+        client = self.get_client()
+        self.collection = client.create_collection(name=self._collection_name)
+        self.collection.add(documents=self._chunks, 
+                            metadatas=[{"source": "hdfc"} for _ in self._chunks], 
+                            ids=[f"id_{i}" for i in range(len(self._chunks))])
+
+
+    def get_client(self):
+        return chromadb.PersistentClient(path="./creditcard_db")
+        #chromadb.Client(Settings(persistent_directory="./creditcard_db"))
+
+
+    def query_results(self, query_text):
+        return self.collection.query(query_texts=query_text, n_results=2 , include=["embeddings", "documents", "metadatas"])
+
+
+def main():
+    # First we will do chunking
+    # Then Embedding
+    cc_data = _read_data()
+    chunks = _perform_chunking(cc_data)
+    chromadb_obj = ChromaDB("HDFC_Credit_Card", [{"source":"internet"}], chunks)
+    chromadb_obj.generate_embeddings()
+    print(f"Embeddings are : {chromadb_obj.query_results('Can I apply for Credit Card?')}")
+
+
+if __name__ == '__main__':
+    main()
